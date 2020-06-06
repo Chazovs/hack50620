fef5f1046b4b8ec7963f3cc5f5b0ff57376b4778
Sergey Chazov <oitv18@gmail.com>
2020-06-06 10:14:53 +0300

    обработка отправки файла

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ParsingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,27 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/', 'ParsingController@index');
+
+
+Route::post('/new/file', function (Request $request) {
+ $content = file_get_contents(public_path('data/001.rtf'));
+ $contentencode = base64_encode($content);
+    $file = $request->file;
+    Storage::put($request->fileName, base64_decode($file));
+
+    $pars          = new  ParsingController;
+    $filePath      = public_path('/app/' . $request->fileName);
+    $resultParsing = $pars->index2($filePath);
+
+
+    $response = [
+        'type'          => 'ДОЛЖНОСТНАЯ ИНСТРУКЦИЯ',
+        'subtype'       => 'АГРОЛЕСОМЕЛИОРАТОР',
+        'path'          => 'Путь в каталоге: instructions/prof/agrolesomeliator/' . $request->fileName,
+        'resultParsing' => $resultParsing,
+    ];
+
+    return json_encode($response);
+
+        /*json_encode($response);*/
+});
