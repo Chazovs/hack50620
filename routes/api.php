0fbb01155175025343a919c7d2bc927f00113f78
Sergey Chazov <oitv18@gmail.com>
2020-06-06 18:15:22 +0300

    начал подключать к серваку с питоном

--- routes/api.php
+++ routes/api.php
@@ -40,21 +40,29 @@ Route::get('/get-py', function () {
 
 Route::post('/new/file', function (Request $request) {
 
+    $name = $request->file->store('/');
+
+    $pars = new  ParsingController;
+    $filePath = public_path('/app/' . $name);
+    $resultParsing = $pars->index2($filePath);
+
+
+    $response = [
+        'status' => 'success',
+        'type' => 'ДОЛЖНОСТНАЯ ИНСТРУКЦИЯ',
+        'subtype' => 'АГРОЛЕСОМЕЛИОРАТОР',
+        'path' => 'instructions/prof/agrolesomeliator/' . $request->fileName,
+        'resultParsing' => $resultParsing,
+    ];
+
+    return json_encode($response);
+});
+
+
+Route::post('/new/file/Whith/Ai', function (Request $request) {
 
     $name = $request->file->store('/');
-    /*$input = $request->all();*/
-    /*$file = $request->file('file');*/
 
-    /*return response(['status'=> 'success', 'message' => $request], 200)
-        ->header('Content-Type', 'text/plain');
-
-
-     $content = file_get_contents(public_path('data/001.rtf'));
-     $contentencode = base64_encode($content);
-        $file = $request->file;*/
-    /*Storage::put($request->fileName, $file);*/
-    /* Storage::put('tmp.rtf', $file);
-     */
     $pars = new  ParsingController;
     $filePath = public_path('/app/' . $name);
     $resultParsing = $pars->index2($filePath);
@@ -70,5 +78,4 @@ Route::post('/new/file', function (Request $request) {
 
     return json_encode($response);
 
-    /*json_encode($response);*/
 });
